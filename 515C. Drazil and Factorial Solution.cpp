1039efb74b4a67d5732b6622456bc05f68b736a5
Arnab Barua <89764073+Arnab-000@users.noreply.github.com>
2023-12-14 03:00:00 +0600

    Codeforces Solution

---
+++ 515C. Drazil and Factorial Solution.cpp
@@ -0,0 +1,65 @@
+// Problem: C. Drazil and Factorial
+// Contest: Codeforces - Codeforces Round 292 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/515/C
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// Author: Catalyst71
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef unsigned long long ull;
+using namespace std;
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define nn endl
+#define sp " "
+#define fx(s) for(auto &x:s)
+#define EPS 1e-10
+#define vl vector<ll>
+#define vi vector<int>
+#define dbp(x) cout << #x << " = " << x << endl
+int dx[8] = {1, -1, 0,  0, 1,  1, -1, -1};
+int dy[8] = {0,  0, 1, -1, 1, -1,  1, -1};
+
+void solve()
+{
+    /* 0 ? 1 ? n ? equal ? */
+    int n;
+    cin >> n;
+    vector<vector<int>> GOAT_CR7 = {{},{},{2},{3},{2,2,3},{5},{5,3},{7},{2,2,2,7},{7,3,3,2}};
+    vector<int> ans; 
+    string s;
+    cin >> s;
+    while(s.size()>0){
+    	int temp = s[(int)s.size()-1]-'0';
+    	s.pop_back();
+    	
+    	if(temp <= 1) continue;
+    	
+    	for(int i = 0; i < (int)GOAT_CR7[temp].size();i++){
+    		ans.pb(GOAT_CR7[temp][i]);
+    	}
+    }
+    sort(rall(ans));
+    fx(ans) cout << x;
+
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+    int t = 1 ;
+    //cin >> t ;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0 ;
+}
