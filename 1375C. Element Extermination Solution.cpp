9418d4cfd4d9dc7bb495689e29a5fecc0a6c6e1f
unknown <arnab.barua.ab@gmail.com>
2024-01-09 13:39:36 +0600

    Codeforces Solution

---
+++ 1375C. Element Extermination Solution.cpp
@@ -0,0 +1,52 @@
+// Problem: C. Element Extermination
+// Contest: Codeforces - Codeforces Global Round 9
+// URL: https://codeforces.com/problemset/problem/1375/C
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// Author: Catalyst71
+
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef unsigned long long ull;
+using namespace std;
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define nn endl
+#define sp " "
+#define fx(s) for (auto &x : s)
+#define EPS 1e-10
+#define vl vector<ll>
+#define vi vector<int>
+#define dbp(x) cout << #x << " = " << x << endl
+int dx[8] = {1, -1, 0, 0, 1, 1, -1, -1};
+int dy[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+
+void solve() {
+  /* 0 ? 1 ? n ? equal ? */
+  int n;
+  cin >> n;
+  vector<int> v(n);
+  fx(v) cin >> x;
+  if (v[0] < v[n - 1])
+    cout << "YES\n";
+  else
+    cout << "NO\n";
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
